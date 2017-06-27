724dca2302e473a2d07c3f832198893c1d5f0f0d
Andrew Liao <andrew.sf.liao@gmail.com>
2017-06-26 22:33:41 -0700

    small change to test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-		System.out.println("You are the best!");
+		System.out.println("You are the best, really!");
 		
 		for (int i = 0; i < 10; i++) {
 			System.out.println("current number is " + i);
